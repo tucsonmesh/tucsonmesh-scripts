d652d2ce1a74ac22a6659c2dd63df23c17df9fb2
Joshua Mack <mackncheesiest@gmail.com>
2022-11-02 18:00:44 -0700

    Updates to omnitik script

--- omnitik-5-ac/flash_omni.sh
+++ omnitik-5-ac/flash_omni.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 clear >"$(tty)"
 
 cat <<EOF
@@ -59,6 +60,7 @@ fi
 config_name="${config_path##*/}"
 
 echo "  Before we can configure the Omnitik, we need to make sure it's powered on and we've switched to the management WiFi network it creates."
+echo "  Alternatively, you can also connect to the Omnitik over Ethernet. Either works."
 echo ""
 echo "  Once you're done with this, press enter to continue..."
 read -r
@@ -73,9 +75,11 @@ fi
 echo "  Looks like the Omnitik is reachable. Proceeding with configuration"
 echo ""
 
+ssh_opts="-o HostKeyAlgorithms=+ssh-rsa -o UserKnownHostsFile=/dev/null"
+
 echo "  Copying firmware file to the Omnitik"
 
-if ! scp -o StrictHostKeyChecking=no "${firmware_path}" admin@192.168.88.1:/; then
+if ! scp ${ssh_opts} "${firmware_path}" admin@192.168.88.1:/; then
   echo "  ERROR: unable to copy firmware (${firmware_path}) to Omnitik!"
   exit 1
 fi
@@ -83,29 +87,24 @@ fi
 echo "  Copying configuration file to the Omnitik"
 echo ""
 
-if ! scp -o StrictHostKeyChecking=no "${config_path}" admin@192.168.88.1:/flash/"${config_name}"; then
+if ! scp ${ssh_opts} "${config_path}" admin@192.168.88.1:/flash/"${config_name}"; then
   echo "  ERROR: unable to copy configuration file (${config_path}) to Omnitik!"
   exit 1
 fi
 
-ssh_cmd="/system reset-configuration no-defaults=yes run-after-reset=/flash/${config_name}"
+ssh_cmd="/system reset-configuration no-defaults=yes run-after-reset=\"flash/${config_name}\""
 
 echo "  Now, before we actually reset the Omnitik, please check that the SSH command we'll execute looks correct"
 echo ""
 echo "${ssh_cmd}"
 echo ""
-echo "  Seem legit? (Y/N): "
+echo -n "  Seem legit? (Y/N): "
 read -r LEGIT
 
 if [ "${LEGIT}" = "Y" ] || [ "${LEGIT}" = "y" ] || [ "${LEGIT}" = "YES" ] || [ "${LEGIT}" = "yes" ]; then
   echo "  Resetting the Omnitik, flashing the new firmware, and updating the configuration..."
-  if ! ssh -o StrictHostKeyChecking=no admin@192.168.88.1 "${ssh_cmd}"; then
-    echo "  ERROR: we got an error code back from SSH when running that command :X"
-  else
-    echo "  The command ran successfully! Now to just wait for the Omnitik to reboot, flash the firmware, apply the new configuration, and sing its little tune"
-  fi
+  ssh ${ssh_opts} admin@192.168.88.1 "${ssh_cmd}"
+  echo "  The command ran successfully (probably, tbh we don't really know). Now to just wait for the Omnitik to reboot, flash the firmware, apply the new configuration, and sing its little tune"
 else
   echo "  Okay, I won't do anything then :("
-  exit 0
 fi
-
